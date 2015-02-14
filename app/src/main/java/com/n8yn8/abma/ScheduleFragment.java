674bf8ee98d2bd5f16e7f82667239cb999fb5bf4
Nathan Condell <nathan@treelineinteractive.com>
2015-02-13 20:44:35 -0800

    Initial commit

---
+++ app/src/main/java/com/n8yn8/abma/ScheduleFragment.java
@@ -0,0 +1,51 @@
+package com.n8yn8.abma;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ScheduleFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ScheduleFragment extends android.support.v4.app.Fragment {
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment ScheduleFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ScheduleFragment newInstance() {
+        ScheduleFragment fragment = new ScheduleFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public ScheduleFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_schedule, container, false);
+    }
+
+
+}
