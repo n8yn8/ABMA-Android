0b5421962e688dcf08c57d3c1abff2ed6cc93593
Nathan Condell <natecondell@gmail.com>
2015-03-21 13:30:56 -0700

    Remove Settings from actionable

--- app/src/main/java/com/n8yn8/abma/MainActivity.java
+++ app/src/main/java/com/n8yn8/abma/MainActivity.java
@@ -109,9 +109,9 @@ public class MainActivity extends ActionBarActivity implements NavigationDrawerF
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
 
         return super.onOptionsItemSelected(item);
     }
