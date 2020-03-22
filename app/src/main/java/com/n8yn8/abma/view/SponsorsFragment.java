9f5f9b9772b411d35aea41cd38633e1ca0dfe256
Nate Condell <natecondell@gmail.com>
2020-03-21 17:32:37 -0700

    Switch to RecyclerView. Fix crash in cache

--- app/src/main/java/com/n8yn8/abma/view/SponsorsFragment.java
+++ app/src/main/java/com/n8yn8/abma/view/SponsorsFragment.java
@@ -3,14 +3,15 @@ package com.n8yn8.abma.view;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import androidx.fragment.app.Fragment;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.GridView;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.n8yn8.abma.R;
 import com.n8yn8.abma.model.AppDatabase;
@@ -35,7 +36,7 @@ public class SponsorsFragment extends Fragment {
      *
      * @return A new instance of fragment SponsorsFragment.
      */
-    public static SponsorsFragment newInstance() {
+    static SponsorsFragment newInstance() {
         SponsorsFragment fragment = new SponsorsFragment();
         Bundle args = new Bundle();
         fragment.setArguments(args);
@@ -57,10 +58,19 @@ public class SponsorsFragment extends Fragment {
 
         View rootView = inflater.inflate(R.layout.fragment_sponsors, container, false);
 
-        final GridView gridview = rootView.findViewById(R.id.gridView);
-        final ImageAdapter imageAdapter = new ImageAdapter(getActivity(), sponsors);
+        final RecyclerView recyclerView = rootView.findViewById(R.id.sponsors_recycler);
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
+        final ImageAdapter imageAdapter = new ImageAdapter(requireActivity(), new ImageAdapter.OnClickListener() {
+            @Override
+            public void onClick(Sponsor sponsor) {
+                if (!TextUtils.isEmpty(sponsor.url)) {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(sponsor.url)));
+                }
+            }
+        });
+        recyclerView.setAdapter(imageAdapter);
 
-        final AppDatabase db = AppDatabase.getInstance(getActivity().getApplicationContext());
+        final AppDatabase db = AppDatabase.getInstance(requireActivity().getApplicationContext());
         final Year year = db.yearDao().getLastYear();
         sponsors.clear();
         sponsors.addAll(db.sponsorDao().getSponsors(year.objectId));
@@ -73,21 +83,12 @@ public class SponsorsFragment extends Fragment {
                     db.sponsorDao().insert(retrievedSponsors);
                     sponsors.clear();
                     sponsors.addAll(retrievedSponsors);
-                    imageAdapter.notifyDataSetChanged();
+                    imageAdapter.submitList(sponsors);
                 }
             });
+        } else {
+            imageAdapter.submitList(sponsors);
         }
-        Log.d("Nate", "setAdapter");
-        gridview.setAdapter(imageAdapter);
-
-        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-                String urlString = sponsors.get(position).url;
-                if (!TextUtils.isEmpty(urlString)) {
-                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(urlString)));
-                }
-            }
-        });
 
         return rootView;
     }
